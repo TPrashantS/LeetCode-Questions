73209f4c2524840433f4110c80e94a67bd04160d
Prashant Kumar <151383063+TPrashantS@users.noreply.github.com>
2026-04-01 02:11:11 +0530

    Time: 0 ms (100%), Space: 42.6 MB (92.51%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -0,0 +1,29 @@
+class Solution {
+
+    public int solve(int curr, int end, int[] nums, int[] dp){
+        if(curr > end){
+            return 0;
+        }
+        if(dp[curr] != -1){
+            return dp[curr];
+        }
+        int opt1 = solve(curr + 1, end, nums, dp);
+        int opt2 = nums[curr] + solve(curr + 2, end, nums, dp);
+        return dp[curr] = Math.max(opt1, opt2);
+    }
+
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if(n == 1) return nums[0];
+        // remove last
+        int[] dp1 = new int[n];
+        Arrays.fill(dp1, -1);
+        int case1 = solve(0, n-2, nums, dp1);
+
+        //remove first
+        int[] dp2 = new int[n];
+        Arrays.fill(dp2, -1);
+        int case2 = solve(1, n-1, nums, dp2);
+        return Math.max(case1, case2);
+    }
+}
