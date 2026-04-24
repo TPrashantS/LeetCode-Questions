4a5f5ff8c0604af46c9a12ccc7fec8829101f96a
Prashant Kumar <151383063+TPrashantS@users.noreply.github.com>
2026-04-24 13:46:05 +0530

    Time: 0 ms (100%), Space: 42.9 MB (39.14%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,32 +1,22 @@
 class Solution {
 
-    public int solve(int curr, int end, int[] nums, int[] dp){
-        if(curr > end){
-            return 0;
-        }
-        if(dp[curr] != -1){
+    public int solve(int curr, int n, int[] nums, int[] dp){
+        if(dp[curr]!=-1){
             return dp[curr];
         }
-        int opt1 = solve(curr + 1, end, nums, dp);
-        int opt2 = nums[curr] + solve(curr + 2, end, nums, dp);
-        return dp[curr] = Math.max(opt1, opt2);
+        if(curr>=n){
+            return 0;
+        }
+        int opt1 = solve(curr + 1, n, nums, dp);
+        int opt2 =nums[curr] + solve(curr+2, n, nums, dp);
+        return dp[curr] = Math.max(opt1,opt2);
     }
 
     public int rob(int[] nums) {
         int n = nums.length;
-
-        if(n == 1) return nums[0];
-
-        // removed last
-        int[] dp1 = new int[n];
-        Arrays.fill(dp1, -1);
-        int case1 = solve(0, n-2, nums, dp1);
-
-        // removed first
-        int[] dp2 = new int[n];
-        Arrays.fill(dp2, -1);
-        int case2 = solve(1, n-1, nums, dp2);
-
-        return Math.max(case1, case2);
+        int[] dp = new int[n+2];
+        Arrays.fill(dp, -1);
+        int ans = Math.max(solve(0, n, nums, dp),solve(1,n,nums,dp));
+        return ans;
     }
 }
